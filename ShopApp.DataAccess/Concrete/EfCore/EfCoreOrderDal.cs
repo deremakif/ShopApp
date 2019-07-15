5328f8f00d027a8c3b7cd02275aac6733397182b
Akif <akif.dere@rlcteknoloji.com>
2019-07-15 17:51:16 +0300

    Listing Products

---
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreOrderDal.cs
@@ -0,0 +1,16 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class EfCoreOrderDal : EfCoreGenericRepository<Order, ShopContext>, IOrderDal
+    {
+
+
+
+
+    }
+}
