b5a0fa9f431ea2f2f79a252a717e2e45eed5a841
Akif <akif.dere@rlcteknoloji.com>
2019-07-16 11:44:54 +0300

    32 - Display Products - 2.

--- ShopApp.WebUI/Controllers/HomeController.cs
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ShopApp.WebUI.Controllers
         {
             return View(new ProductListModel()
             {
-                Products = _productService.GetAll()
+                Products = _productService.GetPopularProducts()
             });
         }
     }
