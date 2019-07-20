ae90885620cfbb0d3db36725297e6b27e12e5a45
Akif <akif.dere@rlcteknoloji.com>
2019-07-20 12:43:17 +0300

    3 - Creating Menu Links

--- ShopApp.WebUI/Models/CategoryListViewModel.cs
+++ ShopApp.WebUI/Models/CategoryListViewModel.cs
@@ -8,6 +8,7 @@ namespace ShopApp.WebUI.Models
 {
     public class CategoryListViewModel
     {
+        public string SelectedCategory { get; set; }
         public List<Category> Categories{ get; set; }
 
     }
--- ShopApp.WebUI/ViewComponents/CategoryListViewComponent.cs
+++ ShopApp.WebUI/ViewComponents/CategoryListViewComponent.cs
@@ -20,6 +20,7 @@ namespace ShopApp.WebUI.ViewComponents
         {
             return View(new CategoryListViewModel()
             {
+                SelectedCategory = RouteData.Values["category"]?.ToString(),
                 Categories = _categoryService.GetAll()
 
         });
