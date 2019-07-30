2e9e9c1a9e189cdaf1f6126a7f2f780d062ed08f
Akif <akif.dere@rlcteknoloji.com>
2019-07-30 18:39:15 +0300

    77 - Order Entities

--- ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -23,7 +23,7 @@ namespace ShopApp.DataAccess.Concrete.EfCore
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Cart> Carts { get; set; }
-        
+        public DbSet<Order> Orders { get; set; }
 
 
     }
